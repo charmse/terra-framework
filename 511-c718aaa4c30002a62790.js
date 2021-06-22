ca4800dd6fff74af526bbcd2de4289b9a5af0af1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-22 19:19:48 +0000

    Deploy cerner/terra-framework to github.com/cerner/terra-framework.git:gh-pages

---
+++ 511-c718aaa4c30002a62790.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[511],{1855:function(e,t,a){"use strict";var l=a(6);Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var n=l(a(0)),o=l(a(342)),r=a(1028),u=n.default.createElement(r.Placeholder,{title:"Test Hero"}),d=function(){return n.default.createElement(o.default,{hero:u})};t.default=d}}]);
